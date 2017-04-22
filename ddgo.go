f921e7d3136ae6d0648e4a5d0ea148b4c0f419e4
nntsugu <nntsugu@gmail.com>
2017-04-23 00:46:26 +0900

    read secrets

--- ddgo.go
+++ ddgo.go
@@ -6,7 +6,28 @@ import (
 	"path/filepath"
 	"io/ioutil"
 	"fmt"
+
+	 "gopkg.in/yaml.v2"
 )
+
+type DatadogKeys struct {
+	Datadog struct {
+		Api_key string
+		App_key string
+	}
+}
+
+type DatadogInformation struct {
+	AuthenticationEP string
+	AuthenticationParams []string
+}
+
+var Datadog DatadogInformation = DatadogInformation{
+	AuthenticationEP: "https://app.datadoghq.com/api/v1/validate",
+	AuthenticationParams:  []string{"?api_key="},
+
+}
+
 
 func main() {
 	targetFile := filepath.Join("..", "secrets", "dd.yaml")
@@ -18,10 +39,22 @@ func main() {
 		fmt.Println(err)
 	}
 
-	str := string(b)
-	fmt.Println(str)
-	//file, _ := os.Open(targetFile)
-	//defer file.Close()
+	// str := string(b)
+	// fmt.Println(str)
+	// fmt.Println(Datadog.AuthenticationEP)
+
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	// fmt.Println("%s", m["datadog"].(map[interface{}]interface{})["api_key"])
+	fmt.Println("%s", m["datadog"])
+	var d DatadogKeys
+	err = yaml.Unmarshal(b, &d)
+	fmt.Println(d.Datadog.Api_key)
 }
 
 func doGet() {
